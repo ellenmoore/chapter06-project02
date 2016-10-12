f7386f50e5c35588cf221977a968879a0f070e0c
Ellen Moore <ellenmoore13@gmail.com>
2016-10-11 20:58:41 -0500

    Update highlightNodes.js

--- highlightNodes.js
+++ highlightNodes.js
@@ -6,9 +6,6 @@ window.onload = function(){
 	for (var i=0; i<elmnt.length; i++){
 		if (elmnt[i].type != 3)
 		{
-		//elmnt.onclick = alert(elmnt.tagName);
-		//console.log(elmnt[i]);
-		//elmnt[i].onclick = alert(elmnt[i].tagName);
 		elmnt[i].addEventListener('click',showTag);
 		}
 		function showTag(){
